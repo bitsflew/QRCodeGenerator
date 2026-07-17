ca56955272491b7d26a0b61bc2a077e6720b6c8c
Henk Kampman <henk.kampman@secondmove.com>
2026-07-17 10:04:31 +0200

    Initial Commit

---
+++ QRCodeGenerator/QRCodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGeneratorApp.swift
+//  QRCodeGenerator
+//
+//  Created by Henk on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
