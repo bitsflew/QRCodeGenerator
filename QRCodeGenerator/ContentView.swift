fe8c5b8be9222eb3926067e9fc1de2899874a910
Henk Kampman <henk.kampman@secondmove.com>
2026-08-19 11:27:08 +0200

    fix: build error

--- QRCodeGenerator/ContentView.swift
+++ QRCodeGenerator/ContentView.swift
@@ -153,26 +153,6 @@ struct ContentView: View {
     }
 }
 
-private enum QRCodeCorrectionLevel: String, CaseIterable {
-    case low = "L"
-    case medium = "M"
-    case quartile = "Q"
-    case high = "H"
-
-    var title: LocalizedStringKey {
-        switch self {
-        case .low:
-            return "Low (7%)"
-        case .medium:
-            return "Medium (15%)"
-        case .quartile:
-            return "Quartile (25%)"
-        case .high:
-            return "High (30%)"
-        }
-    }
-}
-
 #Preview {
     ContentView()
 }
